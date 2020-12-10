be277024e38eca959c1b54622434508e9ae6b412
Nahomie ND <nducasse3@gmail.com>
2020-12-10 14:19:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,17 @@
 require "pry"
 
 def fizzbuzz(int)
- if (int % 3 == 0 ) && (int % 5 == 0 )
-   "FizzBuzz"
+ if int % 3 == 0 
+   "Fizz"
  
-
+ elsif int % 5 == 0
+  "Buzz"
+  
+ elsif int % 3 && 5 == 0
+  "FizzBuzz"
+  
+else
+  "fizzbuzz"
 end
 
 end
